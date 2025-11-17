f0f239fa2d5e528132ecde0f41a13cdd906844a9
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-11-17 08:16:27 +0100

    Test file for calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,27 @@
+import pytest
+from calculator import sum, subtract, multiply, divide
+
+def test_sum():
+    assert sum(2, 3) == 5
+    assert sum(-2, 3) == 1
+    assert sum(-2, -3) == -5
+    assert sum(-2, 0) == -2
+
+def test_subtract():
+    assert subtract(3, 2) == 1
+    assert subtract(-2, 3) == -5
+    assert subtract(-2, -3) == 1
+    assert subtract(-2, 0) == -2
+
+def test_multiply():
+    assert multiply(2,3) == 6
+    assert multiply(-2,3) == -6
+    assert multiply(-2,-3) == 6
+    assert multiply(-2,0) == 0
+
+
+
+
+
+
+# python -m pytest test_calculator.py -v 
