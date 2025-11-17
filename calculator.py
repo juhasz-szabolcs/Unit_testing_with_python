ffe630569e3ef337dbddb49b64c2d1cb8573e43f
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-11-17 08:16:04 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def sum(x, y):
+    return x+y
+
+def subtract(x, y):
+    return x-y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    try:
+        return x / y
+    except ZeroDivisionError:
+        return "Error: Division by zero"
